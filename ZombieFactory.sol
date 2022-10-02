55349e74dc8b1891709c1041def096d9d1ff084e
khadgachy07 <khadgachy07@gmail.com>
2022-10-02 23:06:01 +0545

    Lesson 2

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity >= 0.5.0 < 0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
@@ -14,8 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -25,8 +30,11 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
+
